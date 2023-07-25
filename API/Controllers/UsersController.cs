270288ca85d3994b22b6b99851365781bbc76b38
KammererLara <lara.kammerer@gmail.com>
2023-07-25 20:47:05 +0200

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController] //
+[Route("api/[controller]")] // gibt Route an, worauf dieser Controller hören soll ...hier: /api/users
+public class UsersController : ControllerBase
+{
+    //dependency Injection:
+    private readonly DataContext _context;
+    
+    // Konstruktor für DI nötig
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    
+    // // synchron
+    // // Endpoint
+    // [HttpGet] // --> GET /api/users
+    // // ActionResult ermöglicht mir auch einen Response mit notFound,
+    // // wenn ich nur IEnumerable returne kann ich nur die Liste zurückgeben
+    // public ActionResult<IEnumerable<AppUser>> GetUsers()
+    // {
+    //     var users = _context.Users.ToList(); //fragt schon meine User aus der Users Tabelle der DB ab???
+    //     return users;
+    // }
+
+    // [HttpGet("{id}")]
+    // public ActionResult<AppUser> GetUser(int id)
+    // {
+    //     return _context.Users.Find(id);
+    // }
+    
+    // asynchron
+    // Endpoint
+    [HttpGet] // --> GET /api/users
+    // ActionResult ermöglicht mir auch einen Response mit notFound,
+    // wenn ich nur IEnumerable returne kann ich nur die Liste zurückgeben
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync(); //fragt schon meine User aus der Users Tabelle der DB ab???
+        return users;
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    // Id wird in DB automatisch zu Primary Key, wenn ich einen anders benannten Wert als PK haben will muss ich ein [Key] Attribut dransetzen (davor)
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    
+}
